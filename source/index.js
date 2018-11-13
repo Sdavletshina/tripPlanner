367bce85278ce9f7cf2a748e04b6c59ca059a427
Elizabeth Kosowski <elizabethkosowski@gmail.com>
2018-11-13 17:15:22 -0500

    markers

--- source/index.js
+++ source/index.js
@@ -1,1 +1,17 @@
-console.log('Hello from JavaScript');
+const mapboxgl = require('mapbox-gl');
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiaGlsYXJ5d2VueWkiLCJhIjoiY2ptaTY0MW1qMDFtdDNrbzduNjgzMzlociJ9.Xe1SdmVIO9_VRCMYCecBmQ';
+
+const map = new mapboxgl.Map({
+  container: 'map',
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: 'mapbox://styles/mapbox/streets-v10', // mapbox has lots of different map styles available.
+});
+
+const markerDomEl = document.createElement('div');
+markerDomEl.style.width = '32px';
+markerDomEl.style.height = '39px';
+markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
+new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
